94e8409ba5813d498fbe58537245aba24b4c44c8
maf248 <maf_248@hotmail.com>
2021-08-28 15:46:02 -0300

    Cree los endpoints /api/tourism y filtré acorde los resultados del mismo, además cree el endpoint /api/discount con el filtro apropiado tambien. Faltaría ordenar los resultados y paginarlos.

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,7 +1,46 @@
-var accountsData = require('../data/accounts.json');
+const accountsData = require('../data/accounts.json');
 
 module.exports = {
     index: (req, res, next) => {
-        res.json(accountsData)
+        var response = {
+            meta: {
+                status: 200,
+                total: accountsData.accounts.length
+            },
+            accounts: [...accountsData.accounts]
+        }
+        res.json(response)
+    },
+    tourism: (req, res, next) => {
+        var filterAccountsTags = accountsData.accounts.filter((account)=> {
+            return account.tags.length >= 1;
+        });
+        var filterTourismData = filterAccountsTags.filter((account) => {
+            return account.tags[0].name === "Turismo en Buenos Aires";
+        })
+
+        var response = {
+            meta: {
+                status: 200,
+                total: filterTourismData.length
+            },
+            accounts: filterTourismData
+        }
+        res.json(response)
+    },
+
+    discount: (req, res, next) => {
+        var filterDiscountData = accountsData.accounts.filter((account)=> {
+            return account.haveVoucher;
+        });
+
+        var response = {
+            meta: {
+                status: 200,
+                total: filterDiscountData.length
+            },
+            accounts: filterDiscountData
+        }
+        res.json(response)
     }
 }
--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -8,4 +8,8 @@ const apiController = require('../controllers/apiController');
 // Setting routes
 router.get('/', apiController.index);
 
+router.get('/tourism', apiController.tourism);
+
+router.get('/discount', apiController.discount);
+
 module.exports = router;
