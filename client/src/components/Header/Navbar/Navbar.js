fb9f0918959e1a545c05f9e11b04db7ed4e0c403
maf248 <maf_248@hotmail.com>
2021-08-31 16:16:53 -0300

    Completé el navbar, dandole estado usando hooks y configurando estilos dinamicos en base a dicho estado, para poder ocultar o desplegar el menu del navbar. Funciona correctamente.

--- client/src/components/Header/Navbar/Navbar.js
+++ client/src/components/Header/Navbar/Navbar.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { MenuItems } from './MenuItems';
 import './Navbar.css';
 
-function Navbar () {
+function Navbar() {
+
+    const [toggleNavbar, setToggleNavbar] = useState(true);
+
+    const toggleButton = () => {
+        toggleNavbar ? setToggleNavbar(false) : setToggleNavbar(true);
+    };
+
     return (
             <nav className="navbar">
-                <div className="navbar-top">
-                    <i className="fas fa-bars"></i> <h2 className="navbar-top-title">Ejercicio Club La Nación</h2>
+                <div className="navbar-top" onClick={toggleButton}>
+                    <i className="fas fa-bars"></i> <h2 className={`navbar-top-title ${toggleNavbar ? 'toggle-navbar' : ''}`}>Ejercicio Club La Nación</h2>
                 </div>
-                <ul className="nav-list">
+                <ul className={`nav-list ${toggleNavbar ? 'toggle-navbar' : ''}`}>
                     { MenuItems.map((item, index) => {
                         return (<li className={item.clases} key={`menuitem${index}`}>
                                 <a href={item.url} target="_blank" rel="noreferrer"><i className={item.icon}></i> {item.title}</a>
@@ -16,7 +23,6 @@ function Navbar () {
                     })}
                 </ul>
             </nav>
-    )
+        );
 }
-
 export default Navbar;
