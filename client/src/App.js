dc1a7d88cf29555bf69afa30087b18bd1214a643
maf248 <maf_248@hotmail.com>
2021-09-06 15:41:55 -0300

    Modifique los componentes discountSlider para que reciban la nformacion de la API cada uno en su caso, mediante estado y props.

--- client/src/App.js
+++ client/src/App.js
@@ -35,8 +35,8 @@ class App extends Component {
       <div className='App'>
       <Header/>
       <MainSlider />
-      <DiscountSlider title={'Turismo en Buenos Aires'} btntext={'TODOS LOS BENEFICIOS'}/>
-      <DiscountSlider title={'Códigos de descuento'} subtitle={'¿Sos socio Club LA NACIÓN? Descarga tu código y disfrutá beneficios exclusivos en tus marcas favoritas.'} btntext={'TODOS LOS CÓDIGOS'}/>
+      <DiscountSlider title={'Turismo en Buenos Aires'} btntext={'TODOS LOS BENEFICIOS'} endpoint={'/api/tourism'} />
+      <DiscountSlider title={'Códigos de descuento'} subtitle={'¿Sos socio Club LA NACIÓN? Descarga tu código y disfrutá beneficios exclusivos en tus marcas favoritas.'} btntext={'TODOS LOS CÓDIGOS'} endpoint={'/api/discount'} />
       
       </div>
       <Footer data={!this.state.data ? 'Loading...' : 'Data Ready!'} />
--- client/src/components/DiscountSlider/DiscountSlider.js
+++ client/src/components/DiscountSlider/DiscountSlider.js
@@ -1,10 +1,30 @@
-import React from 'react'
-import './DiscountSlider.css'
+import React from 'react';
+import './DiscountSlider.css';
 
 import Button from '../UI/Button/Button';
+import { useEffect, useState } from 'react'
 
 export default function DiscountSlider(props) {
+    const [isDataReady, setIsDataReady] = useState(false);
+    const [data, setData] = useState(false);
 
+    useEffect(() => {
+        async function getData() {
+            try {
+                const response = await fetch(`${props.endpoint}?page=1&limit=4`);
+                const json = await response.json();
+                setData({...json});
+                setIsDataReady(true);
+                console.log(data)
+                
+            } catch (error) {
+                console.log(error)
+            }
+            
+        }
+        getData();
+        
+    }, [isDataReady]);
 
     return (
         <div className="container-discount-slider">
