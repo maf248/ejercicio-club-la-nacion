8136ae8c9a530b39b5c0d27f6768f26d3ab38c89
maf248 <maf_248@hotmail.com>
2021-09-06 21:16:34 -0300

    Cambié configuracion para poder hacer build en heroku con react y express corriendo en el mismo dominio sobre puertos distintos.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 // Importing modules
 const express = require('express');
-
+const path = require('path');
 // Requiring router
 const apiRouter = require('./routes/apiRouter');
 
@@ -10,8 +10,16 @@ const PORT = process.env.PORT || 3001;
 // Initializing app
 const app = express();
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 // API endpoints routes
 app.use('/api', apiRouter);
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 // App listening port
 app.listen(PORT, () => {
