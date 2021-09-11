503f4b080f7b3513ff4b5728124ca7e71351ddb6
maf248 <maf_248@hotmail.com>
2021-09-11 16:37:28 -0300

    Termine de configurar los cards para que cada uno renderize contenido adecuado, pudiendo reutilizar el componente en ambos sliders siendo que son muy similares diseños. Esta completo los cards de descuentos, faltaria completar los cards de turismo para luego completar la logica de los sliders y finalmente mejorar el CSS y otras mejoras.

--- client/src/components/UI/Card/Card.js
+++ client/src/components/UI/Card/Card.js
@@ -4,31 +4,33 @@ import './Card.css'
 import Button from '../Button/Button';
 
 export default function Card(props) {
-//haveVoucher
+
     const cardInfo = (
                     <>
                     <a href={`https://club.lanacion.com.ar/${props.data.crmid}`} target="_blank" rel="noopener noreferrer">
-                        <p className='card-title'>{props.data.name}</p>
+                        <img className='card-picture' src={props.data.images[0].url} alt={props.data.name} />
                     </a>
+                    <div className='card-data-container'>
+                        <a href={`https://club.lanacion.com.ar/${props.data.crmid}`} target="_blank" rel="noopener noreferrer">
+                            <p className='card-title'>{props.data.name}</p>
+                        </a>
+                    </div>
                     </>
                         );
     const cardButton = (
                     <>
-                    <p className='card-title'>{props.data.name}</p>
-                    <a href={`https://club.lanacion.com.ar/${props.data.crmid}`} target="_blank" rel="noopener noreferrer">
-                        <Button btntext={'QUIERO MI CODIGO'} cardbtn='card-button' />
-                    </a>
+                    <img className='card-picture' src={props.data.images[0].url} alt={props.data.name} />
+                    <div className='card-data-container'>
+                        <p className='card-title'>{props.data.name}</p>
+                        <a href={`https://club.lanacion.com.ar/${props.data.crmid}`} target="_blank" rel="noopener noreferrer">
+                            <Button btntext={'QUIERO MI CODIGO'} cardbtn='card-button' />
+                        </a>
+                    </div>
                     </>
                     );
     return (
         <div className='card-container'>
-            <a href={`https://club.lanacion.com.ar/${props.data.crmid}`} target="_blank" rel="noopener noreferrer">
-                <img className='card-picture' src={props.data.images[0].url} alt={props.data.name} />
-            </a>
-            <div className='card-data-container'>
-                {props.design === 'card-info' ? cardInfo : cardButton }
-                
-            </div>
+            {props.design === 'card-info' ? cardInfo : cardButton }
         </div>
     )
 }
